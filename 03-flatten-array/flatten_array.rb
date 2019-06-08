310ba58d67b866b65dd773e181a0b371ead65fc7
tingtinghsu <tingtinghsu.tw@gmail.com>
2019-06-08 10:26:09 +0900

    flatten array and remove nil without using .flatten and .compact

--- 03-flatten-array/flatten_array.rb
+++ 03-flatten-array/flatten_array.rb
@@ -1,5 +1,12 @@
-module FlattenArray
-  def self.flatten(array)
-    array.flatten.compact
-  end
+class FlattenArray
+  def self.flatten (array, flattened_array = [])
+    array.each do |element|
+      if element.class == Array
+        self.flatten(element, flattened_array)
+      else
+        flattened_array << element unless element.nil?
+      end
+    end
+    flattened_array 
+  end 
 end
