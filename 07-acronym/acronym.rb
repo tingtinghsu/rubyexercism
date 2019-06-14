a5f317b7a915380c08702008a19327e7d6cf67d5
tingtinghsu <tingtinghsu.tw@gmail.com>
2019-06-14 22:30:14 +0900

    condense multiplie-line code to one line

--- 07-acronym/acronym.rb
+++ 07-acronym/acronym.rb
@@ -1,11 +1,5 @@
 class Acronym
   def self.abbreviate(string)
-    puts sting.split.map {|w| w[0]}
-
-    # abbreviation = ""
-    # string.scan(/\w+/){|w| abbreviation += w.slice(0)}
-    # abbreviation.upcase
+    string.scan(/\w+/).map {|w| w[0]}.join.upcase
   end
 end
-
-Acronym.abbreviate('Something - I made up from thin air')
