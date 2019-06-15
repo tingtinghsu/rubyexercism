f862fb47ed0f58774a01c9f76109e985bf6ab5b9
tingtinghsu <tingtinghsu.tw@gmail.com>
2019-06-15 22:59:33 +0900

    use hash to create rna complement

--- 05-rna-transcription/rna_transcription.rb
+++ 05-rna-transcription/rna_transcription.rb
@@ -1,16 +1,8 @@
 class Complement
-  def self.of_dna(dna, rna_complement='')
-      dna.each_char { |char| rna_complement << dna_case(char) }
-      rna_complement
-  end
-
-  def self.dna_case(dna, rna_element='')
-    case dna
-      when 'C' then rna_element << 'G'
-      when 'G' then rna_element << 'C'
-      when 'T' then rna_element << 'A'
-      when 'A' then rna_element << 'U'
-      else rna_element << ''
-    end
+  def self.of_dna(dna)
+    str = ""
+    rna_complement_hash = {'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U'}
+    dna.split('').to_a.map {|x| str += rna_complement_hash[x] }
+    str
   end
 end
