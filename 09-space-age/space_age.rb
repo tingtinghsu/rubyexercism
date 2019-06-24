deb41fc9d9a14bdd705ec7ff0d062f53748d3fb6
tingtinghsu <tingtinghsu.tw@gmail.com>
2019-06-24 21:08:13 +0900

    modified method name

--- 09-space-age/space_age.rb
+++ 09-space-age/space_age.rb
@@ -3,25 +3,25 @@ class SpaceAge
   EARTH_ORBITAL_PERIOD_IN_SECONDS = 31557600.00
 
   ORBITAL_PERIOD_IN_EARTH_YEARS = {
-    "on_mercury" => 0.2408467, 
-    "on_venus" => 0.61519726, 
-    "on_mars" => 1.8808158, 
-    "on_jupiter" => 11.862615, 
-    "on_saturn" => 29.447498, 
-    "on_uranus" => 84.016846, 
-    "on_neptune" => 164.79132
+    "mercury" => 0.2408467, 
+    "venus" => 0.61519726, 
+    "mars" => 1.8808158, 
+    "jupiter" => 11.862615, 
+    "saturn" => 29.447498, 
+    "uranus" => 84.016846, 
+    "neptune" => 164.79132
   }
 
-  def initialize(earth_years_in_seconds)
-    @earth_years_in_seconds = earth_years_in_seconds
+  def initialize(age_in_seconds)
+    @age_in_seconds = age_in_seconds
   end
 
   def on_earth
-    @earth_years_in_seconds / EARTH_ORBITAL_PERIOD_IN_SECONDS
+    @age_in_seconds / EARTH_ORBITAL_PERIOD_IN_SECONDS
   end
 
-  ORBITAL_PERIOD_IN_EARTH_YEARS.each do |on_planet, orbital_period|
-    define_method(on_planet) do
+  ORBITAL_PERIOD_IN_EARTH_YEARS.each do |planet, orbital_period|
+    define_method("on_"+planet) do
       on_earth / orbital_period
     end
   end
